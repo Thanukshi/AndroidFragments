60ebfa44a8c7edaa99db511b60f863aa305256cb
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 13:55:42 +0530

    Using a Fragment

--- app/src/main/java/com/example/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/androidfragments/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.androidfragments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button frag1, frag2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
