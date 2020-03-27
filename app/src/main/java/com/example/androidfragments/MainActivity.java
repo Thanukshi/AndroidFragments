5b7d7c407c19f41626bd7ee5107e9c16da61d525
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 14:01:30 +0530

    Using a Fragment

--- app/src/main/java/com/example/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/androidfragments/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.androidfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         frag2 = findViewById(R.id.btnFrag2_main);
 
     }
-    
+
+    public void changeFragment (View view){
+        Fragment fragment;
+
+        if(view == findViewById(R.id.btnFrag1_main)){
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragmentDefault,fragment);
+            ft.commit();
+        }
+    }
+
 }
