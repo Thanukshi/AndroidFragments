56c4650636167f37fffa0e367b593108f81609dc
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 13:39:42 +0530

    Using a Fragment

---
+++ app/src/main/java/com/example/androidfragments/Fragment1.java
@@ -0,0 +1,24 @@
+package com.example.androidfragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
